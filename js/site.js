90f0cfee151feeb42934d40493fb137dc42227b6
Bryan Cordova <bryancordova.hela@gmail.com>
2024-04-10 18:04:53 -0400

    new comkt

--- js/site.js
+++ js/site.js
@@ -1,5 +1,6 @@
 // collect user input, 
 // entry point of app 
+//first thing in call stack is get values
 function getValues() {
 
     //get the <input> element for startNumber and grab its value
@@ -12,9 +13,32 @@ function getValues() {
     startNumber = Number(startNumber);
     endNumber = Number(endNumber);
 
-    let generatedNumbers = generateValues(startNumber, endNumber);
+    if ( isNaN(startNumber) || isNaN(endNumber)) { //NaN = Not a Number, isNaN checks if the value stored is not a number
+        //display an error message
+        Swal.fire({
+        icon: 'error', 
+        title: 'Oops!', 
+        text: 'Please enter valid numbers for Hundo to use.', 
+        backdrop: false,
+        });
+    }
 
-    displayValues(generatedNumbers);
+    else if ((startNumber > endNumber) || (startNumber < 0) || (endNumber > 100) ) {
+       
+        Swal.fire({
+            icon: 'error', 
+            title: 'Oops!', 
+            text: 'Please make sure start number is less than end number, and that they are between 0-100.', 
+            backdrop: false,
+        });
+    }
+ 
+    else {
+        //display numbers
+        let generatedNumbers = generateValues(startNumber, endNumber);
+        displayValues(generatedNumbers);
+    }
+ 
 
 
 }
@@ -24,7 +48,7 @@ function generateValues(start, end) {
 
     let numbers = [];
 
-    for (let i = start ; i <= end ; i = i + 1){
+    for (let i = start ; i <= end ; i++){
         numbers.push(i);
     }
 
@@ -39,11 +63,24 @@ function displayValues(numberArray) {
 
     let tableHtml = '';
     
-    for(let index = 0; index < numberArray.length; index + 1); {
+    for(let index = 0; index < numberArray.length; index += 1) { //incrementing for loop
 
         let number = numberArray[index];
 
-        tableHtml = tableHtml + '<tr><td>' + number + '</td></tr>';
+        let className='';
+
+        if (number % 2 == 0) { // % = modulus operator
+            // use the class even in the html
+            //boolean logic (true or false)
+            className = 'even';
+        } else {
+            // use the odd class in the html
+            className = 'odd';
+        }
+
+        // tableHtml = tableHtml + '<tr><td>' + number + '</td></tr>';
+        tableHtml += `<tr><td class="${className}">${number}</td></tr>`;//string template literal
+        //${} is string interpolation, in JS neend ${} and back ticks around ur result
     }
 
     let tbody = document.getElementById('results');
